ba98dc27414d45485784ccf9e229afdb80afa029
Jacob Adamson <jakeladamson0@gmail.com>
2022-05-16 23:08:43 -0600

    working on schemas/server side refactor

--- server/server.js
+++ server/server.js
@@ -9,6 +9,15 @@ const db = require("./config/connection");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// setup ApolloServer
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
+
+server.applyMiddleware({ app });
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -17,8 +26,17 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
-app.use(routes);
+// app.use(routes);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 db.once("open", () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`🌍 Now listening on localhost:${PORT}`);
+    console.log(
+      `🌍 Use GraphQL Playground at http://localhost:${PORT}${server.graphqlPath}`
+    );
+  });
 });
