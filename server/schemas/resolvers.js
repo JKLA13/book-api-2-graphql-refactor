3365f6c932694f718be72b5e6db4784b18654d6b
Jacob Adamson <jakeladamson0@gmail.com>
2022-05-16 23:29:10 -0600

    setup resolvers, thought i had scope problems, instead fixed typo

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,66 @@
+const { AuthenticationError } = require("apollo-server-express");
+const { saveBook } = require("../../client/src/utils/API");
+const { User } = require("../models");
+const { signToken } = require("../utils/auth");
+
+const resolvers = {
+  Query: {
+    me: async (parent, args, context) => {
+      if (!context.user) {
+        throw new AuthenticationError("You must be logged in");
+      }
+      const userData = await User.findById(context.user._id);
+      return userData;
+    },
+  },
+
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+      return {
+        token,
+        user,
+      };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+      if (!user) {
+        throw new AuthenticationError("Invalid credentials");
+      }
+      const isValid = await user.isValidPassword(password);
+      if (!isValid) {
+        throw new AuthenticationError("Invalid credentials");
+      }
+      const token = signToken(user);
+      return {
+        token,
+        user,
+      };
+    },
+    saveBook: async (parent, { bookData }, context) => {
+      if (!context.user) {
+        throw new AuthenticationError("You must be logged in");
+      }
+      const updatedUser = await User.findByIdAndUpdate(
+        { _id: context.user._id },
+        { $push: { savedBooks: bookData } },
+        { new: true }
+      );
+      return updatedUser;
+    },
+    removeBook: async (parent, { bookId }, context) => {
+      if (!context.user) {
+        throw new AuthenticationError("You must be logged in");
+      }
+      const updatedUser = await User.findByIdAndUpdate(
+        { _id: context.user._id },
+        { $pull: { savedBooks: { _id: bookId } } },
+        { new: true }
+      );
+      return updatedUser;
+    },
+  },
+};
+
+module.exports = resolvers;
